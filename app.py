c42951e0bc0d4a2d436955656ee29f90fb3b7bd5
sophiembanda <sophiembanda4@gmail.com>
2024-06-05 20:34:37 +0300

    password visibilty

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 def generate_password(length=12):
     characters = string.ascii_letters + string.digits + string.punctuation
     return ''.join(random.choice(characters) for _ in range(length))
+    
 
 # Route for the signup page
 @app.route('/signup', methods=['GET', 'POST'])
@@ -22,6 +23,18 @@ def signup():
         return f"User {username} signed up successfully!"
     # Render the signup page template
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get form data from the request
+        username = request.form['username']
+        password = request.form['password']
+        # confirm_password = request.form['confirm_password']
+        # Add your signup logic here (e.g., save the user to the database)
+        return f"User {username} logged in successfully!"
+    # Render the signup page template
+    return render_template('login.html')
 
 # Route to generate a random password and return it as a JSON response
 @app.route('/generate-password')
